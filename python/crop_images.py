ff4f3c06cd9ab6f10409fad33b2fee9150989d68
Brandon C. Kelly <bckelly80@gmail.com>
2014-02-20 09:05:32 -0800

    working on gaussian fitting of sources

--- python/crop_images.py
+++ python/crop_images.py
@@ -3,18 +3,21 @@ __author__ = 'brandonkelly'
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-from astropy.modeling import models, fitting
 import os
 import glob
 from skimage.feature import peak_local_max
+import pandas as pd
+from scipy import optimize
+import datetime
 
 base_dir = os.environ['HOME'] + '/Projects/Kaggle/galaxy_zoo/'
 data_dir = base_dir + 'data/'
 training_dir = data_dir + 'images_training_rev1/'
 test_dir = data_dir + 'images_test_rev1/'
+plot_dir = base_dir + 'plots/'
 
 training_files = glob.glob(training_dir + '*.jpg')
-doplot = True
+doshow = True
 
 
 # Define a function to make the ellipses
@@ -29,70 +32,206 @@ def ellipse(ra, rb, ang, x0, y0, Nb=100):
     return X, Y
 
 
+def arctanh(x):
+    z = 0.5 * np.log((1.0 + x) / (1.0 - x))
+    return z
+
+
+def tanh(z):
+    x = (np.exp(2.0 * z) - 1.0) / (np.exp(2.0 * z) + 1.0)
+    return x
+
+
+def bivariate_gaussian(x, y, x_sigma, y_sigma, rho, xcent, ycent):
+    xsqr = (x - xcent) ** 2 / x_sigma ** 2
+    ysqr = (y - ycent) ** 2 / y_sigma ** 2
+    xysqr = (x - xcent) * (y - ycent) / (x_sigma * y_sigma)
+    bgauss = np.exp(-0.5 / (1.0 - rho ** 2) * (xsqr + ysqr - 2.0 * rho * xysqr))
+    return bgauss
+
+
+def sum_of_gaussians(xgrid, ygrid, params, xcent, ycent):
+    """
+    Sum of Gaussian functions model, with the centroids held fixed.
+
+    :param xgrid, ygrid: The x and y values, npix ** 2 element arrays.
+    :param params: The sequence of parameters for the K gaussian functions. Each Gaussian function will have parameters
+        (log(amplitude), log(x_sigma), log(y_sigma), arctanh(rho)).
+    :param xcent: The mean for the x-values.
+    :param ycent: The mean for the y-values.
+    """
+    ngauss = len(xcent)
+    model = 0.0
+    for k in xrange(ngauss):
+        amp = np.exp(params[4 * k])
+        x_sigma = np.exp(params[4 * k + 1])
+        y_sigma = np.exp(params[4 * k + 2])
+        rho = tanh(params[4 * k + 3])
+        model += amp * bivariate_gaussian(xgrid, ygrid, x_sigma, y_sigma, rho, xcent[k], ycent[k])
+
+    return model
+
+
+def sum_of_gaussians_error(params, image, xgrid, ygrid, xcent, ycent):
+    error = sum_of_gaussians(xgrid, ygrid, params, xcent, ycent) - image
+    return error
+
+
+error_messages = {'SourceID': [], 'Band': [], 'ErrorFlag': []}
+
 print 'Doing file...'
-for file in training_files[1:10]:
+for file in training_files[0:10]:
     print file, '...'
+    source_id = file.split('/')[-1].split('.')[0]
     # load the JPEG image into a numpy array
     im = np.array(Image.open(file)).astype(float)
     ndim = im.shape
 
+    df_list = []
+
     # loop over images in each band
+    start_time = datetime.datetime.now()
     for c in range(3):
         print '    ', c, '...'
         # find local maximum that is closest to center of image
         this_im = im[:, :, c]
-        coords = peak_local_max(this_im, min_distance=20)
+        flux_sigma = 1.5 * np.median(np.abs(this_im - np.median(this_im)))  # MAD: robust estimate of sigma
+        peak_threshold = np.median(this_im) + 5.0 * flux_sigma  # only look for 5-sigma peaks
+        coords = peak_local_max(this_im, min_distance=50, threshold_abs=peak_threshold, exclude_border=False)
+        uvals, uidx = np.unique(this_im[coords[:, 0], coords[:, 1]], return_index=True)
+        coords = coords[uidx, :]  # only keep peaks with unique flux values
         distance = np.sqrt((coords[:, 0] - ndim[0] / 2) ** 2 + (coords[:, 1] - ndim[1] / 2) ** 2)
-        lmax_idx = distance.argmin()
-        centroid = coords[lmax_idx, :]  # coordinates of galaxy center
+        # order the peaks by distance from the center of the image
+        sort_idx = np.argsort(distance)
+        distance = distance[sort_idx]
+        coords = coords[sort_idx, :]
 
-        # now find approximate galaxy radius by fitting a 2-d gaussian function
-        xcent = np.arange(this_im.shape[0]) - centroid[0]
-        ycent = np.arange(this_im.shape[1]) - centroid[1]
-        Mxx = np.sum(xcent ** 2 * this_im[:, centroid[1]]) / np.sum(this_im[:, centroid[1]])
-        Mxx = min(Mxx, (ndim[0] / 4.0) ** 2)
-        Myy = np.sum(ycent ** 2 * this_im[centroid[0], :]) / np.sum(this_im[centroid[0], :])
-        Myy = min(Myy, (ndim[1] / 4.0) ** 2)
+        # fit a mixture of gaussian functions model to the image, one gaussian for each local maximum
+        nsources = len(distance)
+        iparams = np.zeros(nsources * 4)
+        xcentroids = np.zeros(nsources)
+        ycentroids = np.zeros(nsources)
+        for i in xrange(nsources):
+            centroid = coords[i, :]
+            xcentroids[i] = centroid[0]
+            ycentroids[i] = centroid[1]
+            xcent = np.arange(ndim[0]) - centroid[0]
+            ycent = np.arange(ndim[1]) - centroid[1]
+            Mxx = np.sum(xcent ** 2 * this_im[:, centroid[1]]) / np.sum(this_im[:, centroid[1]])
+            Myy = np.sum(ycent ** 2 * this_im[centroid[0], :]) / np.sum(this_im[centroid[0], :])
+            if i == 0:
+                # main galaxy, so make initial guess of sigmas no larger than 1/4 length of image
+                Mxx_min = (ndim[0] / 4.0) ** 2
+                Myy_min = (ndim[1] /4.0) ** 2
+            else:
+                # contaminants, make make initial guess of sigmas no larger than 20 pixels
+                Mxx_min = 20.0 ** 2
+                Myy_min = 20.0 ** 2
+            Mxx = min(Mxx, Mxx_min)
+            Myy = min(Myy, Myy_min)
 
-        g2d_init = models.Gaussian2D(amplitude=1.0, x_mean=centroid[0], y_mean=centroid[1], y_stddev=np.sqrt(Myy),
-                                     x_stddev=np.sqrt(Mxx))
-        g2d_init.x_mean.fixed = True
-        g2d_init.y_mean.fixed = True
-        fitter = fitting.NonLinearLSQFitter()
-        x, y = np.mgrid[:ndim[0], :ndim[1]]
-        gauss_fit = fitter(g2d_init, x, y, this_im)
+            amp = this_im[centroid[0], centroid[1]]
+
+            iparams[4 * i] = np.log(amp)
+            iparams[4 * i + 1] = 0.5 * np.log(Mxx)
+            iparams[4 * i + 2] = 0.5 * np.log(Myy)
+
+        # subtract off the base level of the image as the median of the values along the border
+        border = np.hstack((this_im[:, 0], this_im[:, -1], this_im[0, 1:-1], this_im[-1, 1:-1]))
+        base_flux = np.median(border)
+
+        image_fit = this_im - base_flux
+        xgrid, ygrid = np.mgrid[:ndim[0], :ndim[1]]
+
+        params, success = optimize.leastsq(sum_of_gaussians_error, iparams,
+                                           args=(image_fit.ravel(), xgrid.ravel(), ygrid.ravel(),
+                                                 xcentroids, ycentroids), ftol=1e-2, xtol=1e-2)
+
+        # if error, then save the info for analysis later
+        if success not in [1, 2, 3, 4]:
+            error_messages['SourceID'].append(source_id)
+            error_messages['Band'].append(c)
+            error_messages['ErrorFlag'].append(success)
+
+        # get ellipse parameters for each Gaussian function
+        rotang = np.zeros(nsources)
+        amajor = np.zeros(nsources)
+        aminor = np.zeros(nsources)
+        gidx = 4 * np.arange(nsources)
+        rho = tanh(params[gidx + 3])
+        xsigma = np.exp(params[gidx + 1])
+        ysigma = np.exp(params[gidx + 2])
+        for k in range(nsources):
+            covar = np.zeros((2, 2))
+            covar[0, 0] = xsigma[k] ** 2
+            covar[1, 1] = ysigma[k] ** 2
+            covar[0, 1] = rho[k] * xsigma[k] * ysigma[k]
+            covar[1, 0] = covar[0, 1]
+
+            vals, vecs = np.linalg.eigh(covar)
+            order = vals.argsort()[::-1]
+            vals, vecs = vals[order], vecs[:, order]
+
+            rotang[k] = np.arctan2(*vecs[:, 0][::-1])
+            amajor[k], aminor[k] = 2 * np.sqrt(vals)  # major and minor axes of ellipse
+
+        # convert parameter output to a dictionary
+        gauss_params = {'amplitude': np.exp(params[gidx]), 'xcent': xcentroids, 'ycent': ycentroids,
+                        'xsigma': xsigma, 'ysigma': ysigma, 'rho': rho, 'theta': np.degrees(rotang),
+                        'amajor': amajor, 'aminor': aminor}
+        gauss_params = pd.DataFrame(gauss_params)  # convert to Pandas DataFrame
+        gauss_params.index.name = 'GaussianID'
+        df_list.append(gauss_params)
 
         # crop the image to 3-sigma and save it
-        xrange = 4.0 * np.abs(gauss_fit.x_stddev)
-        yrange = 4.0 * np.abs(gauss_fit.y_stddev)
-        xmin = int(centroid[0] - xrange)
+        xrange = 4.0 * np.abs(gauss_params['xsigma'][0])
+        yrange = 4.0 * np.abs(gauss_params['ysigma'][0])
+        xmin = int(coords[0, 0] - xrange)
         if xmin < 0:
             xmin = 0
-        xmax = int(centroid[0] + xrange)
+        xmax = int(coords[0, 0] + xrange)
         if xmax > ndim[0]:
             xmax = ndim[0]
-        ymin = int(centroid[1] - yrange)
+        ymin = int(coords[0, 1] - yrange)
         if ymin < 0:
             ymin = 0
-        ymax = int(centroid[1] + yrange)
+        ymax = int(coords[0, 1] + yrange)
         if ymax > ndim[1]:
             ymax = ndim[1]
 
         cropped_im = this_im[xmin:xmax, ymin:ymax]  # crop the image
 
-        if doplot:
-            # show the image
-            plt.subplot(121)
-            plt.imshow(this_im, cmap='hot')
-            plt.title('Original')
-            ra = min(2 * np.abs(gauss_fit.x_stddev), ndim[0] - centroid[0])
-            rb = min(2 * np.abs(gauss_fit.y_stddev), ndim[1] - centroid[1])
-            x_ell, y_ell = ellipse(ra, rb, gauss_fit.theta, gauss_fit.x_mean, gauss_fit.y_mean)
+        # save a plot of the image with the Gaussian ellipses and the cropped image
+        plt.subplot(121)
+        plt.imshow(this_im, cmap='hot')
+        plt.title('Original')
+        for k in range(nsources):
+            # plot centroid of each ellipse
+            plt.plot(xcentroids[k], ycentroids[k], 'bo')
+            # plot 2-sigma region of each gaussian
+            x_ell, y_ell = ellipse(amajor[k], aminor[k], rotang[k], xcentroids[k], ycentroids[k])
             plt.plot(x_ell, y_ell, 'b')
-            plt.subplot(122)
-            plt.imshow(cropped_im, cmap='hot')
-            plt.title('Cropped')
-            # create the 2-sigma ellipse of the Gaussian
+        plt.xlim(0, ndim[0])
+        plt.ylim(0, ndim[1])
+        plt.subplot(122)
+        plt.imshow(cropped_im, cmap='hot')
+        plt.title('Cropped')
+        plt.savefig(plot_dir + source_id + '_' + str(c) + '.png')
+        if doshow:
             plt.show()
 
-        # finally, save the image
+        # TODO: still need to median filter anything outside of 4-sigma
+
+        # finally, save the cropped image as a numpy array
+        np.save(training_dir + source_id + '_' + str(c), cropped_im)
+
+    # save the mixture of gaussians model parameters
+    dataframe = pd.concat(df_list, keys=['1', '2', '3'])
+    dataframe.to_csv(data_dir + source_id + '_gauss_params.csv')
+
+    end_time = datetime.datetime.now()
+
+    tdiff = end_time - start_time
+    tdiff = tdiff.total_seconds()
+    print 'Doing', tdiff, 'seconds / galaxy.'
+    print training_files * tdiff / 60.0 / 60.0, 'hours left.'
