c6c2249996fd7f95eaadebfde6ead1f89d48c14b
Brandon C. Kelly <bckelly80@gmail.com>
2014-03-24 16:38:44 -0700

    add constraints when calculating OOB error

--- python/randomforest_fit.py
+++ python/randomforest_fit.py
@@ -7,7 +7,6 @@ import glob
 from sklearn.ensemble import RandomForestRegressor
 import cPickle
 import pandas as pd
-from dct_to_lda import image_sanity_check
 
 base_dir = os.environ['HOME'] + '/Projects/Kaggle/galaxy_zoo/'
 data_dir = base_dir + 'data/'
@@ -19,7 +18,30 @@ verbose = True
 njobs = 1
 
 
-def train_rf(df, y):
+def get_rmse(y, yfit):
+    # calculate remaining classes using constraints
+    yfit['Class1.3'] = 1.0 - yfit['Class1.1'] - yfit['Class1.2']
+    yfit['Class2.2'] = yfit['Class1.2'] - yfit['Class2.1']
+    yfit['Class3.2'] = yfit['Class2.2'] - yfit['Class3.1']
+    yfit['Class4.2'] = yfit['Class2.2'] - yfit['Class4.1']
+    yfit['Class5.4'] = yfit['Class2.2'] - yfit['Class5.1'] - yfit['Class5.2'] - \
+                            yfit['Class5.3']
+    yfit['Class6.2'] = 1.0 - yfit['Class6.1']
+    yfit['Class7.3'] = yfit['Class1.1'] - yfit['Class7.1'] - yfit['Class7.2']
+    yfit['Class8.7'] = yfit['Class6.1'] - yfit['Class8.1'] - yfit['Class8.2'] - \
+                            yfit['Class8.3'] - yfit['Class8.4'] - yfit['Class8.5'] - \
+                            yfit['Class8.6']
+    yfit['Class9.3'] = yfit['Class2.1'] - yfit['Class9.1'] - yfit['Class9.2']
+    yfit['Class10.3'] = yfit['Class4.1'] - yfit['Class10.1'] - yfit['Class10.2']
+    yfit['Class11.6'] = yfit['Class4.1'] - yfit['Class11.1'] - yfit['Class11.2'] - \
+                             yfit['Class11.3'] - yfit['Class11.4'] - yfit['Class11.5']
+
+    rmse = np.sqrt((y - yfit) ** 2)
+    assert np.isfinite(rmse)
+    return rmse
+
+
+def train_rf(df, y, ntrees=None, msplit=None):
 
     # Random Forest predictions Correspond to these values. Need to calculate the values for the remaining
     # classes using the summation constraints.
@@ -28,66 +50,75 @@ def train_rf(df, y):
                    'Class8.6', 'Class9.1', 'Class9.2', 'Class10.1', 'Class10.2', 'Class11.1', 'Class11.2',
                    'Class11.3', 'Class11.4', 'Class11.5']
 
+    y_unique = y[unique_cols]
+
     # first find optimal number of trees
-    ntrees = [10, 20, 40, 80, 160, 320]
-    oob_rmse = np.zeros(len(ntrees))
-    for i, nt in enumerate(ntrees):
-        rf = RandomForestRegressor(max_features='sqrt', oob_score=True, n_estimators=nt, verbose=verbose, n_jobs=njobs)
-        rf.fit(df.values, y.values)
-        yhat_oob = rf.oob_prediction_
-        oob_rmse[i] = np.sqrt(np.mean((yhat_oob - y) ** 2))
+    if ntrees is None:
+        ntrees = [10, 20, 40, 80, 160, 320]
+        oob_rmse = np.zeros(len(ntrees))
+        for i, nt in enumerate(ntrees):
+            rf = RandomForestRegressor(max_features='sqrt', oob_score=True, n_estimators=nt, verbose=verbose,
+                                       n_jobs=njobs)
+            rf.fit(df.values, y_unique.values)
+            yhat_oob = pd.DataFrame(data=rf.oob_prediction_, index=y.index, columns=unique_cols)
+            oob_rmse[i] = get_rmse(y, yhat_oob)
 
-    if verbose:
-        print '# of trees | OOB RMSE'
-        for i in range(len(ntrees)):
-            print ntrees[i], '    | ', oob_rmse[i]
-        print ''
-        print 'Optimal number of trees is', ntrees[oob_rmse.argmin()]
+        if verbose:
+            print '# of trees | OOB RMSE'
+            for i in range(len(ntrees)):
+                print ntrees[i], '    | ', oob_rmse[i]
+            print ''
+            print 'Optimal number of trees is', ntrees[oob_rmse.argmin()]
 
-    plt.clf()
-    plt.plot(ntrees, oob_rmse, '-o')
-    plt.ylabel('OOB RMSE')
-    plt.xlabel('Number of Trees')
-    plt.savefig(plot_dir + 'OOB_RMSE_RF_ntrees.png')
-    if doshow:
-        plt.show()
-        plt.close()
+        plt.clf()
+        plt.plot(ntrees, oob_rmse, '-o')
+        plt.ylabel('OOB RMSE')
+        plt.xlabel('Number of Trees')
+        plt.savefig(plot_dir + 'OOB_RMSE_RF_ntrees.png')
+        if doshow:
+            plt.show()
+            plt.close()
 
-    ntrees = ntrees[oob_rmse.argmin()]
+        ntrees = ntrees[oob_rmse.argmin()]
 
-    # now find optimum value of m (features to consider in split)
-    nfeatures = [5, 10, 20, 40, 80, 160]
+    if msplit is None:
+        # now find optimum value of m (features to consider in split)
+        msplit = [5, 10, 20, 40, 80, 160]
 
-    oob_rmse = np.zeros(len(nfeatures))
-    best_rf = None
-    best_rmse = 1e300
-    for i, m in enumerate(nfeatures):
-        rf = RandomForestRegressor(max_features=m, oob_score=True, n_estimators=ntrees, verbose=verbose, n_jobs=njobs)
-        rf.fit(df.values, y)
-        yhat_oob = rf.oob_prediction_
-        oob_rmse[i] = np.sqrt(np.mean((yhat_oob - y) ** 2))
-        if oob_rmse[i] < best_rmse:
-            # save best RF so we don't need to recompute it later
-            best_rf = rf
-            best_rmse = oob_rmse[i]
+        oob_rmse = np.zeros(len(msplit))
+        best_rf = None
+        best_rmse = 1e300
+        for i, m in enumerate(msplit):
+            rf = RandomForestRegressor(max_features=m, oob_score=True, n_estimators=ntrees, verbose=verbose,
+                                       n_jobs=njobs)
+            rf.fit(df.values, y_unique.values)
+            yhat_oob = pd.DataFrame(data=rf.oob_prediction_, index=y.index, columns=unique_cols)
+            oob_rmse[i] = get_rmse(y, yhat_oob)
+            if oob_rmse[i] < best_rmse:
+                # save best RF so we don't need to recompute it later
+                best_rf = rf
+                best_rmse = oob_rmse[i]
 
-    if verbose:
-        print 'm features | OOB RMSE'
-        for i in range(len(nfeatures)):
-            print nfeatures[i], '    | ', oob_rmse[i]
-        print ''
-        print 'Optimal number of features for split is', nfeatures[oob_rmse.argmin()]
+        if verbose:
+            print 'm features | OOB RMSE'
+            for i in range(len(msplit)):
+                print msplit[i], '    | ', oob_rmse[i]
+            print ''
+            print 'Optimal number of features for split is', msplit[oob_rmse.argmin()]
 
-    plt.clf()
-    plt.plot(nfeatures, oob_rmse, '-o')
-    plt.ylabel('OOB RMSE')
-    plt.xlabel('Number of Feature for Split Criteria')
-    plt.savefig(plot_dir + 'OOB_RMSE_RF_mfeatures.png')
-    if doshow:
-        plt.show()
-        plt.close()
+        plt.clf()
+        plt.plot(msplit, oob_rmse, '-o')
+        plt.ylabel('OOB RMSE')
+        plt.xlabel('Number of Feature for Split Criteria')
+        plt.savefig(plot_dir + 'OOB_RMSE_RF_mfeatures.png')
+        if doshow:
+            plt.show()
+            plt.close()
+    else:
+        best_rf = RandomForestRegressor(max_features=msplit, oob_score=True, n_estimators=ntrees, verbose=verbose,
+                                        n_jobs=njobs)
+        best_rf.fit(df.values, y_unique)
 
-    mfeatures = nfeatures[oob_rmse.argmin()]
     cPickle.dump(best_rf, open(data_dir + 'RF_regressor.pickle', 'wb'))
 
     # make feature importance plot
